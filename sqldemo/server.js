9c71070db07b843d2a4f68080fd1daf7477a4035
programmingwheels <leninlawrence111@gmail.com>
2021-11-30 15:46:02 +0530

    SQL demo

---
+++ sqldemo/server.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express();
+const bodyParser = require("body-parser")
+const knex = require("./config/db.config")
+const { v4: uuidv4 } = require('uuid');
+
+app.use(bodyParser.json())
+
+const port = 3001
+
+
+app.post("/players", async (req, res) => {
+    console.log(req.body);
+    try {
+        req.body.id = uuidv4();
+        await knex('player').insert(req.body);
+        res.json({ message: 'Player inserted' })
+    } catch (err) {
+        res.status(400).json({ errMsg: err.message })
+    }
+
+})
+
+app.get("/players", async (req, res) => {
+    try {
+        req.body.id = uuidv4();
+        let data = await knex.select().table('player')
+        res.json({ data: data })
+    } catch (err) {
+        res.status(400).json({ errMsg: err.message })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
